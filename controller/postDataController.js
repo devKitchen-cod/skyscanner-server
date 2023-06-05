e16cd47752b10a57b9d184072bb7e1531f33936b
SergeyZup <sergey@zup.ee>
2023-06-05 19:13:57 +0300

    add AddFlight endpoint

--- controller/postDataController.js
+++ controller/postDataController.js
@@ -1,23 +1,31 @@
-const flightModel = require("../models/flightModel")
+const flightModel = require("../models/flightModel");
 
-module.exports.addCountry = async function(req, res){
- 
-    try {
-      const city = await cityModel.find({})
-      return res.status(200).json(city)
-    } catch (error) {
-      console.log(error)
-      req.status(500).json(error)
-    }
+module.exports.addCountry = async function (req, res) {
+  try {
+    const city = await cityModel.find({});
+    return res.status(200).json(city);
+  } catch (error) {
+    console.log(error);
+    req.status(500).json(error);
   }
+};
 
-  module.exports.addFlight = async function(req, res){
-    try {
-      console.log('[req]', req)
-      const flight = await flightModel.create({
-        
-      })
-    } catch (error) {
-      console.log('error', error)
-    }
+module.exports.addFlight = async function (req, res) {
+  const { obj } = req.body;
+  try {
+    console.log("[req]", obj);
+    const flight = await flightModel.create({
+      origin_airport: obj.origin_airport,
+      distination_airport: obj.distination_airport,
+      origin_city: obj.origin_city,
+      distination_city: obj.distination_city,
+      price: obj.price,
+      origin_time: obj.origin_time,
+      distination_time: obj.distination_time,
+      flight_time: obj.flight_time,
+    });
+    res.status(200).json("Good");
+  } catch (error) {
+    console.log("error", error);
   }
+};
--- models/flightModel.js
+++ models/flightModel.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const flightModel = new mongoose.Schema({
-   
-  origin: {type: String, ref: 'Airport'},
-  destination: {type: String, ref: 'Airport'},
-  flight_time: {type: String},
-  origin_time: {type: String},
-  distination_time: {type: String},
-  default_price: {type: String},
-  
-})
-module.exports = mongoose.model('Flight', flightModel)
+  origin_airport: { type: String },
+  distination_airport: { type: String },
+  origin_city: { type: String },
+  distination_city: { type: String },
+  flight_time: { type: String },
+  origin_time: { type: String },
+  distination_time: { type: String },
+  price: { type: String },
+});
+module.exports = mongoose.model("Flight", flightModel);
