b4442ee89915b9fc27197c05d86f35c4c67601fd
Sergey <smagometov649@gmail.com>
2023-09-28 10:24:14 +0300

    fix routing

--- routes/user-route.js
+++ routes/user-route.js
@@ -2,27 +2,33 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../controller/authController')
-const getDataController = require('../controller/getDataController')
-const postDataController = require('../controller/postDataController')
+
+const countriesController = require('../controller/countries_controller')
+// const getDataController = require('../controller/getDataController')
+// const postDataController = require('../controller/postDataController')
 // const serviceAppController = require('../controller/serviceAppController')
-const search = require('../controller/users_controllers/search_controller')
+// const search = require('../controller/users_controllers/search_controller')
 
 router.post('/auth-user-create', authController.createUser)
 router.post('/auth-user-update', authController.updateUser)
 router.post('/auth-user-login', authController.loginUser)
 router.post('/getadmin', authController.getAdmin)
-router.post('/addFlight', postDataController.addFlight)
-router.post('/getFindedAirports', getDataController.getFindAirports)
-router.post('/getCity', getDataController.getCity)
-router.post('/getDirection', getDataController.getDirection)
 
-router.post('/search', search.Search)
+// router.post('/addFlight', postDataController.addFlight)
+// router.post('/getFindedAirports', getDataController.getFindAirports)
+// router.post('/getCity', getDataController.getCity)
+// router.post('/getDirection', getDataController.getDirection)
 
-router.get('/getCountry', getDataController.getCountry)
-router.get('/getAirports', getDataController.getAirpots)
-router.get('/getFlights', getDataController.getFlights)
-router.get('/getAirportsByCity', getDataController.getFindAirport)
-router.get('/updateAirports', getDataController.updateAirports)
+// router.post('/search', search.Search)
+
+router.get('/getAllCountries', countriesController.getAllCountries)
+router.post('/getCountryById', countriesController.getCountryById)
+
+  // router.get('/getCountry', getDataController.getCountry)
+  // router.get('/getAirports', getDataController.getAirpots)
+  // router.get('/getFlights', getDataController.getFlights)
+  // router.get('/getAirportsByCity', getDataController.getFindAirport)
+  // router.get('/updateAirports', getDataController.updateAirports)
 
 // router.get('/serviceFunction', serviceAppController.RelocateData)
 
